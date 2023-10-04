1fcc93ff112d339998db7aa1b7c1fef1bfd11fa1
bd <bd@linuxdev.myguest.virtualbox.org>
2023-10-04 11:02:17 -0400

    added new AutoCleanup class to include/jmg/util.h

--- include/jmg/util.h
+++ include/jmg/util.h
@@ -19,4 +19,19 @@ const auto& value_of(const auto& rec) {
 auto& value_of(auto& rec) {
   return std::get<1>(rec);
 }
+
+template<typename Fcn>
+struct AutoCleanup
+{
+  AutoCleanup(Fcn&& fcn) : fcn_(std::move(fcn)) {}
+  ~AutoCleanup() {
+    if (isActive_) { fcn_(); }
+  }
+  void cancel() { isActive_ = false; }
+
+private:
+  bool isActive_ = true;
+  Fcn fcn_;
+};
+
 } // namespace jmg
