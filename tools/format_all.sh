f36f0ab42eba1df4a91130442c0ca87722d46532
Brian Davis <brian8702@sbcglobal.net>
2025-07-19 18:51:41 -0400

    added format_all.sh tool to format all C++ code automatically

---
+++ tools/format_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO(bd) use a hermetic version of clang-format?
+
+# format all C++ code using rules provided in .clang-format
+find . -type f -name '*.h' -o -name '*.cpp' | xargs clang-format -i
