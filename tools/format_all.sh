8164265f946643dd2aa1b10291cdd3386b9377b4
Brian Davis <brian8702@sbcglobal.net>
2025-07-26 06:44:26 -0400

    ignore errors in formatting script

--- tools/format_all.sh
+++ tools/format_all.sh
@@ -3,4 +3,4 @@
 # TODO(bd) use a hermetic version of clang-format?
 
 # format all C++ code using rules provided in .clang-format
-find . -type f -name '*.h' -o -name '*.cpp' | xargs clang-format -i
+find . -type f -name '*.h' -o -name '*.cpp' | xargs clang-format -i 2>/dev/null
