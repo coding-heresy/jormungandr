716ee98d5376144ea997738c7e4af83269e99f04
Brian Davis <brian8702@sbcglobal.net>
2026-04-12 14:23:03 -0400

    added first cut of new stream/chunk serialization interface

---
+++ include/jmg/stream_io.h
@@ -0,0 +1,73 @@
+/** -*- mode: c++ -*-
+ *
+ * Copyright (C) 2026 Brian Davis
+ * All Rights Reserved
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Brian Davis <brian8702@sbcglobal.net>
+ *
+ */
+#pragma once
+
+#include <concepts>
+#include <optional>
+#include <span>
+
+#include "jmg/types.h"
+
+namespace jmg
+{
+
+class OutputStreamBuffer {
+public:
+  explicit OutputStreamBuffer(std::span<Octet> buf);
+
+  std::span<Octet> getNextChunk(size_t sz);
+
+  void consume(std::optional<size_t> sz = std::nullopt);
+
+  size_t remaining() const;
+
+  size_t consumed() const;
+
+private:
+  // index of the the first octet in the buffer that has not been requested
+  size_t idx_ = 0;
+  // index of the the first octet in the buffer that has not been consumed
+  size_t consumed_ = 0;
+  std::span<Octet> buf_;
+};
+
+template<typename T>
+concept ChunkedOutputStreamT =
+  requires(T strm, size_t sz, std::optional<size_t> opt_sz) {
+    // T::getNextChunk takes a size and returns a chunk
+    { strm.getNextChunk(sz) } -> std::convertible_to<std::span<Octet>>;
+
+    // T::consume takes an optional size and returns void
+    { strm.consume(opt_sz) } -> std::same_as<void>;
+  };
+
+} // namespace jmg
